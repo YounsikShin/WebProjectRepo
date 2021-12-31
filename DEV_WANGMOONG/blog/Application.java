42e218b16544de839c8c7ad07c99e4c074bba931
Wangmoong <tjzmf23@gmail.com>
2021-12-31 23:25:58 +0900

    before

---
+++ DEV_WANGMOONG/blog/Application.java
@@ -0,0 +1,7 @@
+package blog;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("My First JAVA");
+    }
+}
--- DEV_WANGMOONG/blog/src/pages/introduce.jsx
+++ DEV_WANGMOONG/blog/src/pages/introduce.jsx
@@ -3,7 +3,11 @@ import styles from './css/app.module.css';
 
 class Introduce extends React.Component {
   render() {
-    return <div className={styles.content}>자기소개 페이지 개발중 입니다.</div>;
+    return (
+      <div className={styles.content}>
+        <p>자기소개 페이지 개발중 입니다.</p>
+      </div>
+    );
   }
 }
 
